3ec75d725d21cf703f19e709cf792c31a322732c
Asadullah Khan <asadullah178100@gmail.com>
2024-06-23 07:07:21 -0800

    done

--- app.js
+++ app.js
@@ -6,3 +6,9 @@ const form = document.querySelector("#form");
 const city = document.querySelector("#city");
 const card = document.querySelector("#card");
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  axios(
+    `http://api.weatherapi.com/v1/current.json?key=b4db485d7c4c485fa6d84351232508&q=${city.value}&aqi=no`
+  )
+    
