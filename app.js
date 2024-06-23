1b93936a72acc259b7c5c4edd2a529efa4a33adc
Asadullah Khan <asadullah178100@gmail.com>
2024-06-23 07:04:17 -0800

    done

---
+++ app.js
@@ -0,0 +1,8 @@
+// b4db485d7c4c485fa6d84351232508
+
+// http://api.weatherapi.com/v1/current.json?key=b4db485d7c4c485fa6d84351232508&q=London&aqi=no
+
+const form = document.querySelector("#form");
+const city = document.querySelector("#city");
+const card = document.querySelector("#card");
+
